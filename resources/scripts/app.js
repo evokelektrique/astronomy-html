72e69b58af44f1937e36b2e522c08f1e538ad2d9
EVOKE <evoke.lektrique@gmail.com>
2021-08-31 15:47:30 +0430

    Add panels

---
+++ resources/scripts/app.js
@@ -0,0 +1,27 @@
+
+function reset_panels(panels) {
+   panels.forEach(panel => {
+      panel.style.display = "none";
+   })
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+   // Panels
+   const panel_links = document.querySelectorAll(".custom-panel-link")
+   const panels      = document.querySelectorAll(".is-custom-panel")
+
+   reset_panels(panels)
+
+   Array.from(panel_links).forEach(link => {
+      link.addEventListener("click", (e) => {
+         e.preventDefault()
+         reset_panels(panels)
+         const panel = document.getElementById(e.target.dataset.target)
+         panel.style.display = "block"
+         panel.scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
+      })
+   })
+
+
+
+})
--- webpack.mix.js
+++ webpack.mix.js
@@ -3,3 +3,4 @@ const mix = require('laravel-mix');
 mix.setPublicPath('public/')
 mix.setResourceRoot('../')
 mix.sass('resources/styles/app.scss', 'public/css')
+mix.js('resources/scripts/app.js', 'public/js')
